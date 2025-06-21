6cd356cb441e05d26f94f6b1caca962bceb4a22c
Ra_Ma_Dan <helmzhotramadan@gmail.com>
2025-06-21 15:55:14 +0100

    Delete pages directory

--- pages/About.jsx
+++
@@ -1,9 +0,0 @@
-function About(){
-    
-
-    return <h1 className="bg-black text-white text-[4rem] text-center m-4 font-bold">
-        This is The About Page
-    </h1>
-}
-
-export default About
